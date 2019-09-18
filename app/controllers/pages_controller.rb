b9a38890153f92d9f09e3f2a4e2b1aea37429efe
mac fyfe <macspersonalemail@gmail.com>
2019-09-17 20:01:35 -0500

    hw3 done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,12 @@ class PagesController < ApplicationController
         end
     end
     def question
+        q = "Which of the following is the single responsability principle?"
+        answers = ["A class should have only one reaseon to change",
+        "Each dev on a team should have one main responsability",
+        "Every app will have one responsability that is most important to users"] 
         respond_to do |format|
-            format.html { render :question}
+            format.html { render :question, locals: {q: q, answers: answers} }
         end
     end
 end
